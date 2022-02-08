a686cea132705e3b9889a2173ff0dd5f9f951980
Lucas <40960766+lucascbarbosa@users.noreply.github.com>
2022-02-08 15:14:45 -0300

    updates controle_adap

--- trabalho-1/controle_adap_script.m
+++ trabalho-1/controle_adap_script.m
@@ -2,7 +2,7 @@ close all;
 clear all;
 
 % reference
-ref_pulse_T = 10;
+ref_pulse_T = 18;
 
 ref_sin_freq = 1/ref_pulse_T;
 
@@ -20,3 +20,20 @@ bp = 1;
 % parameters
 alpha = 1;
 beta = 1;
+
+out = sim('controle_adap',ref_pulse_T+2);
+
+
+%optimal k and theta
+k_opt = bm/bp;
+theta_opt = (am-ap)/bp;
+
+% Plots
+% y,ym and r
+plot(out.y)
+hold on
+plot(out.ym)
+hold on
+plot(out.r)
+title({'$y(t),y_m(t),r(t)$'},'Interpreter','latex')
+xtitle({'$t$'},'Interpreter','latex')
