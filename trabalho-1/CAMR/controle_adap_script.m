dab1b67057b4560a839fbf7441d3e294a00bf8f8
Lucas <40960766+lucascbarbosa@users.noreply.github.com>
2022-02-18 16:53:11 -0300

    finished trabalho-1

--- trabalho-1/CAMR/controle_adap_script.m
+++ trabalho-1/CAMR/controle_adap_script.m
@@ -6,7 +6,7 @@ ref_pulse_T = 18;
 
 ref_sin_freq = 1/ref_pulse_T;
 
-switch_ref = 1;
+switch_ref = 0;
 ref = ["pulse" "sine"];
 ref = ref(switch_ref+1);
 
@@ -40,7 +40,9 @@ hold on
 plot(out.r)
 legend({'$y(t)$','$y_m(t)$','$r(t)$'},'Interpreter','latex')
 xlabel({'$t$'},'Interpreter','latex')
+title('Saída do sistema');
 saveas(gcf,"imgs/yymr_"+ref+".png");
+
 % k and theta
 figure;
 plot(out.K)
@@ -50,5 +52,13 @@ yline(k_opt,'-','$K^*$','Interpreter','latex');
 yline(theta_opt,'-','$\theta^*$','Interpreter','latex');
 legend({'$K$','$\theta$'},'Interpreter','latex');
 xlabel({'$t$'},'Interpreter','latex');
+title('Ganhos adaptativos');
 saveas(gcf,"imgs/ktheta_"+ref+".png");
+
+% v
+figure;
+plot(out.v);
+xlabel({'$t$'},'Interpreter','latex');
+title('Sinal de controle');
+saveas(gcf,"imgs/v_"+ref+".png");
 
